78f6c0542e99ca063abff7bf652a7378257e2d22
Max Barren <csgold2002@mail.ru>
2024-04-10 14:45:18 +0300

    fin

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const button1 = document.getElementById("firstFilter");
     const button2 = document.getElementById("secondFilter");
     button1.addEventListener("click", () => {
+        // if ((button1.textContent = "Select")) {
+        //     button1.textContent = "Clear filter";
+        //     button2.setAttribute("disabled", "");
+        // }
+        // if ((button1.textContent = "Clear filter")) {
+        //     button1.textContent = "Select";
+        //     button2.removeAttribute("disabled");
+        // }
         button1.style.backgroundColor = "green";
         button2.style.backgroundColor = "white";
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -9,6 +17,13 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
     button2.addEventListener("click", () => {
+        // if ((button2.textContent = "Clear filter")) {
+        //     button1.textContent = "Select";
+        //     button2.textContent = "Select";
+        // } else if ((button2.textContent = "Select")) {
+        //     button1.textContent = "Clear filter";
+        //     button2.textContent = "Clear filter";
+        // }
         button1.style.backgroundColor = "white";
         button2.style.backgroundColor = "green";
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
